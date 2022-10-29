560194cdc2085cd28424ed4a196c2e2901d06893
VietBac <81665443+vietbac0967@users.noreply.github.com>
2022-10-29 20:01:12 +0700

    Đọc dữ liệu danh sách hóa đơn

--- BHTT/src/dao/DAO_ChiTietHoaDon.java
+++ BHTT/src/dao/DAO_ChiTietHoaDon.java
@@ -1,4 +1,3 @@
-
 package dao;
 
 import connectDB.ConnectDB;
@@ -19,16 +18,16 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 
-
-
 public class DAO_ChiTietHoaDon {
-    
-    private DAO_NhaCungCap dao_ncc= new DAO_NhaCungCap();
-    private DAO_LoaiSP dao_lsp= new DAO_LoaiSP();
-    private DAO_MauSac dao_ms= new DAO_MauSac();
-    private DAO_ChatLieu dao_cl= new DAO_ChatLieu();
-    private DAO_KichThuoc dao_kt= new DAO_KichThuoc();
+
+    private DAO_NhaCungCap dao_ncc = new DAO_NhaCungCap();
+    private DAO_LoaiSP dao_lsp = new DAO_LoaiSP();
+    private DAO_MauSac dao_ms = new DAO_MauSac();
+    private DAO_ChatLieu dao_cl = new DAO_ChatLieu();
+    private DAO_KichThuoc dao_kt = new DAO_KichThuoc();
+    private DAO_HoaDon hd_dao = new DAO_HoaDon();
     private ArrayList<ChiTietHoaDon> dsCTHD;
+   
     public ArrayList<ChiTietHoaDon> getallDSHoaDon() {
         dsCTHD = new ArrayList<ChiTietHoaDon>();
         try {
@@ -38,20 +37,50 @@ public class DAO_ChiTietHoaDon {
             Statement statement = con.createStatement();
             ResultSet rs = statement.executeQuery(sql);
             while (rs.next()) {
-                  int soLuong = rs.getInt("soLuong");
-                  double VAT =  rs.getDouble("VAT");
-                  HoaDon maHD = new HoaDon(rs.getString("maHD"));
-                  SanPham maSP = new SanPham(rs.getString("maSP"));
-                  double tongTien = rs.getDouble("tongTien");
-                  double tienThoi = rs.getDouble("tienThoi");             
-                  ChiTietHoaDon cthd = new ChiTietHoaDon(soLuong, VAT, tongTien, tienThoi, maHD, maSP);
-                  dsCTHD.add(cthd); 
+                int soLuong = rs.getInt("soLuong");
+                double VAT = rs.getDouble("VAT");
+                HoaDon maHD = new HoaDon(rs.getString("maHD"));
+                SanPham maSP = new SanPham(rs.getString("maSP"));
+                double tongTien = rs.getDouble("tongTien");
+                double tienThoi = rs.getDouble("tienThoi");
+                ChiTietHoaDon cthd = new ChiTietHoaDon(soLuong, VAT, tongTien, tienThoi, maHD, maSP);
+                dsCTHD.add(cthd);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return dsCTHD;
     }
+    
+    public ArrayList<ChiTietHoaDon> layDSHoaDonLenBang() {
+      ArrayList<ChiTietHoaDon> dsCTHDX = new ArrayList<ChiTietHoaDon>();
+        try {
+            ConnectDB.getInstance();
+            Connection con = ConnectDB.getConnection();
+            String sql = """
+                         select hd.maHD,hd.ngayLap,SUM(cthd.soLuong) as TongSoLuong,SUM(cthd.tongTien) as TongThanhTien, nv.tenNV
+                                                  from HoaDon hd
+                                                  join ChiTietHoaDon cthd on cthd.maHD = hd.maHD
+                                                  join NhanVien nv on nv.maNV = hd.maNV
+                                                  group by hd.ngayLap,hd.maHD,nv.tenNV""";
+            Statement statement = con.createStatement();
+            ResultSet rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                int soLuong = rs.getInt("TongSoLuong");
+                double VAT = 0.1;
+                HoaDon maHD = hd_dao.layHoaDonTheoMa(rs.getString("maHD"));
+                SanPham maSP = new SanPham("SP001");
+                double tongTien = rs.getDouble("TongThanhTien");
+                double tienThoi = 1000;
+                ChiTietHoaDon cthd = new ChiTietHoaDon(soLuong, VAT, tongTien, tienThoi, maHD, maSP);
+                dsCTHDX.add(cthd);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dsCTHDX;
+    }
+   
     public boolean themCTHD(ChiTietHoaDon cthd) {
         ConnectDB.getInstance();
         Connection con = ConnectDB.getConnection();
@@ -59,12 +88,12 @@ public class DAO_ChiTietHoaDon {
         int n = 0;
         try {
             stmt = con.prepareStatement("insert into ChiTietHoaDon values(?,?,?,?,?,?)");
-            stmt.setInt(1,cthd.getSoLuong());
-            stmt.setDouble(2,cthd.getVAT());
-            stmt.setString(3,cthd.getHoaDon().getMaHD());
-            stmt.setString(4,cthd.getSanPham().getMaSP());
-            stmt.setDouble(5,cthd.getTongTien());
-            stmt.setDouble(6,cthd.getTienThoi());
+            stmt.setInt(1, cthd.getSoLuong());
+            stmt.setDouble(2, cthd.getVAT());
+            stmt.setString(3, cthd.getHoaDon().getMaHD());
+            stmt.setString(4, cthd.getSanPham().getMaSP());
+            stmt.setDouble(5, cthd.getTongTien());
+            stmt.setDouble(6, cthd.getTienThoi());
             n = stmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -77,44 +106,43 @@ public class DAO_ChiTietHoaDon {
         }
         return n > 0;
     }
-    
-   public SanPham laySanPhamBangMa(String maTim) {
-        
-        try(
-             java.sql.Connection con = ConnectDB.opConnection();
-            PreparedStatement pts = con.prepareStatement("Select * from SanPham where maSP = ?  ")){
-            pts.setString(1,maTim );
-                try(ResultSet rs = pts.executeQuery()){
-                    if (rs.next()){
-                     String maSP= rs.getString("maSP");
-                String tenSP= rs.getString("tenSP"); 
-                double gia= rs.getDouble("giaGoc");
-                int sl= rs.getInt("soLuong");
-                byte[] hinhanh= rs.getBytes("hinhAnh");
-                String moTa= rs.getString("moTa");
-                java.sql.Date ngayN= rs.getDate("ngayNhap");
-                Date ngayNhap = new Date(ngayN.getTime());
-                
-                java.sql.Date han= rs.getDate("hanSD");
-                Date hanSD = new Date(han.getTime());
-                String maNCC= rs.getString("maNCC");
-                String mau= rs.getString("maMau");
-                String kt= rs.getString("maKichThuoc");
-                String loiSP= rs.getString("maLoaiSP");
-                NhaCungCap ncc = dao_ncc.layNhaCungCapBangMa(maNCC);
-                LoaiSanPham lsp = dao_lsp.layLoaiSPBangMa(loiSP);
-                ChatLieu chatl= dao_cl.layChatLieuBangMa(rs.getString("maChatLieu"));
-                MauSac ms= dao_ms.layMauSacBangMa(rs.getString("maMau"));
-                KichThuoc kicht = dao_kt.layKichThuocBangMa(kt);
-                LoaiSanPham loaiSP= new LoaiSanPham(loiSP);
-                SanPham sp = new SanPham(maSP, tenSP, gia, sl, hinhanh, moTa, ngayNhap, hanSD, ncc, chatl, ms, kicht, lsp);
-                        return sp;
-                    }
+
+    public SanPham laySanPhamBangMa(String maTim) {
+
+        try (
+                 java.sql.Connection con = ConnectDB.opConnection();  PreparedStatement pts = con.prepareStatement("Select * from SanPham where maSP = ?  ")) {
+            pts.setString(1, maTim);
+            try ( ResultSet rs = pts.executeQuery()) {
+                if (rs.next()) {
+                    String maSP = rs.getString("maSP");
+                    String tenSP = rs.getString("tenSP");
+                    double gia = rs.getDouble("giaGoc");
+                    int sl = rs.getInt("soLuong");
+                    byte[] hinhanh = rs.getBytes("hinhAnh");
+                    String moTa = rs.getString("moTa");
+                    java.sql.Date ngayN = rs.getDate("ngayNhap");
+                    Date ngayNhap = new Date(ngayN.getTime());
+
+                    java.sql.Date han = rs.getDate("hanSD");
+                    Date hanSD = new Date(han.getTime());
+                    String maNCC = rs.getString("maNCC");
+                    String mau = rs.getString("maMau");
+                    String kt = rs.getString("maKichThuoc");
+                    String loiSP = rs.getString("maLoaiSP");
+                    NhaCungCap ncc = dao_ncc.layNhaCungCapBangMa(maNCC);
+                    LoaiSanPham lsp = dao_lsp.layLoaiSPBangMa(loiSP);
+                    ChatLieu chatl = dao_cl.layChatLieuBangMa(rs.getString("maChatLieu"));
+                    MauSac ms = dao_ms.layMauSacBangMa(rs.getString("maMau"));
+                    KichThuoc kicht = dao_kt.layKichThuocBangMa(kt);
+                    LoaiSanPham loaiSP = new LoaiSanPham(loiSP);
+                    SanPham sp = new SanPham(maSP, tenSP, gia, sl, hinhanh, moTa, ngayNhap, hanSD, ncc, chatl, ms, kicht, lsp);
+                    return sp;
                 }
-            }catch(Exception e){
-                e.printStackTrace();
-       }
-       return null;
-     }
-   
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
